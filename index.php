ef0f59a7a7b7887d61725966eacca9d624ff6ab9
fcamposleonardo <camposleonardo18@gmail.com>
2023-04-03 23:29:34 -0300

    Atualiza Logo

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leos Trader</title>
+</head>
+<body>
+    <p>Hello word</p>
+    <img src="assets\img\leoscities-low-resolution-logo-black-on-transparent-background.png" alt="">
+
+</body>
+</html>
