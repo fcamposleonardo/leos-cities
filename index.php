4207834a9bf953ab587351c0641b6b3120b85ba3
fcamposleonardo <camposleonardo18@gmail.com>
2023-04-13 19:59:34 -0300

    cria navbar

--- index.php
+++ index.php
@@ -4,11 +4,67 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Leos Trader</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/cafc52a010.js" crossorigin="anonymous"></script>
+    <title>Leos Cities</title>
 </head>
 <body>
-    <p>Hello word</p>
-    <img src="assets\img\leoscities-low-resolution-logo-black-on-transparent-background.png" alt="">
+  <header>
+    <nav class="navbar branca" positiion="absolute" width="1439px" height="141px" left="0px" top="4px" style="background-color: white">
+    <div class="row">
+    <img src="assets\img\leoscities-low-resolution-logo-black-on-transparent-background.png" class="img-fluid" alt="" position="absolute" width="172px" height="81px" left="0px" top="0px">
+    
+    <p> <font size="2"><br> Green Cities | Smart Infrastructure | Real Estate | Public Private Partnership</font></p>
+  </div>
+      <div class="socialMedia mr-5"position="absolute" width="108px" height="60px" left="153px" top="43px">
+        <i class="fa-brands fa-facebook fa-xl" style="color: #000000;"></i>
+        <i class="fa-brands fa-linkedin fa-xl" style="color: #000000;"></i>
+      </div>
+    </nav>
+  
 
+  </header>
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+
+
+
+
+
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">
+          
+        </h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae inventore quae mollitia culpa tempora maxime consequuntur illo soluta velit ut maiores, fugiat reiciendis ad? Quasi fuga eos voluptate deserunt expedita!
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
