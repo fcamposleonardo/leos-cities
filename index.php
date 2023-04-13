5643af4637e09340afbbdc1366442226543d6bbb
fcamposleonardo <camposleonardo18@gmail.com>
2023-04-13 20:21:35 -0300

    Atualiza Identação

--- index.php
+++ index.php
@@ -1,36 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <script src="https://kit.fontawesome.com/cafc52a010.js" crossorigin="anonymous"></script>
-    <title>Leos Cities</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <script src="https://kit.fontawesome.com/cafc52a010.js" crossorigin="anonymous"></script>
+  <title>Leos Cities</title>
 </head>
+
 <body>
   <header>
-    <nav class="navbar branca" positiion="absolute" width="1439px" height="141px" left="0px" top="4px" style="background-color: white">
-    <div class="row">
-    <img src="assets\img\leoscities-low-resolution-logo-black-on-transparent-background.png" class="img-fluid" alt="" position="absolute" width="172px" height="81px" left="0px" top="0px">
-    
-    <p> <font size="2"><br> Green Cities | Smart Infrastructure | Real Estate | Public Private Partnership</font></p>
-  </div>
-      <div class="socialMedia mr-5"position="absolute" width="108px" height="60px" left="153px" top="43px">
+    <nav class="navbar branca" positiion="absolute" width="1439px" height="141px" left="0px" top="4px"
+      style="background-color: white">
+      <div class="row">
+        <img src="assets\img\leoscities-low-resolution-logo-black-on-transparent-background.png" class="img-fluid"
+          alt="" position="absolute" width="172px" height="81px" left="0px" top="0px">
+        <p>
+          <font size="2"><br> Green Cities | Smart Infrastructure | Real Estate | Public Private Partnership</font>
+        </p>
+      </div>
+      <div class="socialMedia mr-5" position="absolute" width="108px" height="60px" left="153px" top="43px">
         <i class="fa-brands fa-facebook fa-xl" style="color: #000000;"></i>
         <i class="fa-brands fa-linkedin fa-xl" style="color: #000000;"></i>
       </div>
     </nav>
-  <nav class="navbar azul" position ="absolute" width="1434px height="48px" left="0px" top="141px" style="background-color: blue">
+    <nav class="navbar azul" position="absolute" width="1434px height=" 48px" left="0px" top="141px"
+      style="background-color: blue">
 
-  </nav>
-  
+    </nav>
+
 
   </header>
-    <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
+  <!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+    Launch demo modal
+  </button>
 
 
 
@@ -38,36 +45,46 @@
 
 
 
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">
-          
-        </h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae inventore quae mollitia culpa tempora maxime consequuntur illo soluta velit ut maiores, fugiat reiciendis ad? Quasi fuga eos voluptate deserunt expedita!
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">
+
+          </h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae inventore quae mollitia culpa tempora
+          maxime consequuntur illo soluta velit ut maiores, fugiat reiciendis ad? Quasi fuga eos voluptate deserunt
+          expedita!
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
 
 
 
 
 
 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+    crossorigin="anonymous"></script>
 </body>
+
 </html>
