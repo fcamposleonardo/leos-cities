71858f0c474c33372d2c9f9a3c59c11499e742e7
fcamposleonardo <camposleonardo18@gmail.com>
2023-04-15 22:16:47 -0300

    script php adicionado

--- index.php
+++ index.php
@@ -7,14 +7,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="assets\css\styles.css" href="">
+    <link rel="stylesheet" href="assets\css\style.css">
   <script src="https://kit.fontawesome.com/cafc52a010.js" crossorigin="anonymous"></script>
   <title>Leos Cities</title>
 </head>
+  
 
 <body>
   <header>
-    <nav class="navbar bg-body-tertiary">
+    <nav class="navbar bg-body-tertiary" style="background-color:black">
       <div class="container-fluid">
         <a class="navbar-brand" href="#">
           <img src="assets\img\leoscities-low-resolution-logo-black-on-transparent-background.png" class="img-fluid"
@@ -25,9 +26,7 @@
           <a class="nav-link" href="#">Smart Infrastructure</a>
           <a class="nav-link" href="#">Real Estate</a>
           <a class="nav-link" href="#">Public Private Partnership</a>
-        </nav><!-- <p>
-          <font size="2"><br> Green Cities | Smart Infrastructure | Real Estate | Public Private Partnership</font>
-        </p> -->
+        </nav>
         <div class="socialMedia mr-5">
           <i class="fa-brands fa-facebook fa-xl" style="color: #000000;"></i>
           <i class="fa-brands fa-linkedin fa-xl" style="color: #000000;"></i>
@@ -61,35 +60,16 @@
       </li>
     </ul>
   </header>
-  
-    <!-- <nav class="navbar-branca">
-      <div class="row">
-        <img src="assets\img\leoscities-low-resolution-logo-black-on-transparent-background.png" class="img-fluid"
-          alt="" position="absolute" width="172px" height="81px" left="0px" top="0px">
-        <p>
-          <font size="2"><br> Green Cities | Smart Infrastructure | Real Estate | Public Private Partnership</font>
-        </p>
-      </div>
-      <div class="socialMedia mr-5">
-        <i class="fa-brands fa-facebook fa-xl" style="color: #000000;"></i>
-        <i class="fa-brands fa-linkedin fa-xl" style="color: #000000;"></i>
-      </div>
-    </nav>
-    <nav class="navbar azul bg-blue" position="absolute" width="1434px height=" 48px" left="0px" top="141px"
-      style="background-color: blue"> -->
+    
 
-  </header>
+
+
+
+
   <!-- Button trigger modal -->
   <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
     Launch demo modal
   </button>
-
-
-
-
-
-
-
   <!-- Modal -->
   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
     aria-hidden="true">
@@ -115,7 +95,59 @@
       </div>
     </div>
   </div>
-
+<footer>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <img src="assets\img\leoscities-low-resolution-logo-color-on-transparent-background.png" class="img-fluid" alt="" position="absolute" width="197px" height="92px" left="0px" top="-20px">
+      <a class="nav-link" href="#">Briding Oportunities</a>
+      <a class="nav-link" href="#">Building Solutions</a>
+      <i class="fa-brands fa-facebook fa-xl" style="color: #000000;"></i>
+      <i class="fa-brands fa-linkedin fa-xl" style="color: #000000;"></i>
+    </div>
+    <div class="col">
+      <div class="col">                                                 
+      <a class="nav-link" href="#">Our Services</a>
+      </div>
+      <div class="col">
+      <a class="nav-link" href="#">Design</a>
+      <a class="nav-link" href="#">Feasibility</a>
+      <a class="nav-link" href="#">Financing</a>
+      <a class="nav-link" href="#">Partnership</a>
+      <a class="nav-link" href="#">Legal</a>
+      </div>
+    </div>
+    <div class="col">
+      <div class="col">                                                 
+        <a class="nav-link" href="#">Quick Links</a>
+      </div>
+      <div class="row row-cols-2 row-cols-lg-2 g-2 g-lg-2">
+    <div class="col">
+      <div class="p-3">Home</div>
+    </div>
+    <div class="col">
+      <div class="p-3">FAQs</div>
+    </div>
+    <div class="col">
+      <div class="p-3">About Us</div>
+    </div>
+    <div class="col">
+      <div class="p-3">Clients</div>
+    </div>
+    <div class="col">
+      <div class="p-3">Services</div>
+    </div>
+    <div class="col">
+      <div class="p-3">Contact Us</div>
+    </div>
+    <div class="col">
+      <div class="p-3">Blogs</div>
+    </div>
+  </div>
+    </div>
+  </div>
+</div>
+</footer>
 
 
 
@@ -127,6 +159,16 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
     crossorigin="anonymous"></script>
+
+    <script>
+      console.log('Hello World JS!')
+      let var1=4;
+      let var2=10;
+      const var3=2;
+
+      console.log('resultado', var1 * (var2 + var3));
+
+    </script>
 </body>
 
 </html>
